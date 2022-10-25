c46890cb53de77801cf64e80d9c77de59b93e883
G-RIMA <gikandimaria178@gmail.com>
2022-10-25 22:51:18 +0300

    set up app

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
 const Home = () => {
+  
+  const [userInput, setUserInput] = useState('')
+  const handleChange = (e) => {
+    e.preventDefault()
 
-
+    setUserInput(e.target.value)
+    console.log(userInput)
+  }
   return (
     <div>
       <h1>Todo</h1>
       <form>
-        <input type="text"/>
+        <input type="text" onChange={handleChange}/>
         <button>Add</button>
       </form>
     </div>
