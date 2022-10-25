c50d5d6ad2709e57f4b6414d74b419a7cf660cd5
G-RIMA <gikandimaria178@gmail.com>
2022-10-25 23:19:26 +0300

    enter values to todo list

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,34 @@ import { useState } from 'react'
 const Home = () => {
   
   const [userInput, setUserInput] = useState('')
+  const [todoList, setTodoList] = useState([])
   const handleChange = (e) => {
     e.preventDefault()
 
     setUserInput(e.target.value)
-    console.log(userInput)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTodoList([
+      userInput,
+      ...todoList
+    ])
   }
   return (
     <div>
       <h1>Todo</h1>
       <form>
         <input type="text" onChange={handleChange}/>
-        <button>Add</button>
+        <button onClick={handleSubmit}>Add</button>
       </form>
+      <ul>
+        {
+          todoList.length >= 1 ? todoList.map ((todo,idx) => {
+            return <li key={idx}>{todo}</li>
+          })
+          :'Enter a todo Item'
+        }
+      </ul>
     </div>
   )
 }
